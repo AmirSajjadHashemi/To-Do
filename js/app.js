d0bc63912b325f526119e48db321659cd3e26082
DariushGreat <amirsajjad.hashemi@outlook.com>
2023-02-28 23:45:50 +0330

    Create task generator / Create add to localStorage / Create get from localStorage \n Remove task function changed / Add windo load function to generate from localStorage

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ let addNewBTN = _query ('#add-new')
 let addNewField = _query ('#add-new-field')
 let addNewFieldInput = _query ('#add-new-field input')
 let ToDoArray = []
-let count = 1
 
 /**
  * Functions
@@ -21,44 +20,76 @@ function openAddNewInput () {
     addNewField.classList.remove ('hide')
 }
 
+function addToLocalStorage (input_Array) {
+    localStorage.setItem ('to-do', JSON.stringify(input_Array))
+}
+
+function getFromLocalStorage () {
+    return JSON.parse(localStorage.getItem ('to-do'))
+}
+
 function addToDoHandler () {
+
     let newTask = {
-        id: count,
+        id: ToDoArray.length + 1,
         title: addNewFieldInput.value,
         status: 'incomplete',
     }
     ToDoArray.push (newTask)
-    localStorage.setItem ('to-do', JSON.stringify(ToDoArray))
-    toDoGenerator (addNewFieldInput.value, ToDoArray[count-1].id, ToDoArray[count-1].status)
-    count ++
+    
+    addToLocalStorage (ToDoArray)
+    // taskList.innerHTML = ''
+    toDoGenerator (ToDoArray)
+    
 }
 
-function toDoGenerator (input_string, id, status) {
+function toDoGenerator (input_Array) {
     addNewField.classList.add ('hide')
     addNewBTN.classList.remove ('hide')
     let listBody = $.createElement ('div')
     listBody.className = 'body'
-    let liTask = `
-    <li class="task" id="${id}" status="${status}">
-        <span class="material-icons">drag_indicator</span>
-        <h4>${input_string}</h4>
-        <div class="editable">
-            <span class="edit">
-                <span class="material-icons">edit</span>
-            </span>
-            <span class="delete" onclick="removeThisTask (event)">
-                <span class="material-icons">delete_forever</span>
-            </span>
-        </div>
-    </li>
-    `
-    listBody.insertAdjacentHTML ('afterbegin', liTask)
+    input_Array.forEach ( function (taskItem) {
+        let liTask = `
+        <li class="task" data-id="${taskItem.id}" status="${taskItem.status}">
+            <span class="material-icons">drag_indicator</span>
+            <h4>${taskItem.title}</h4>
+            <div class="editable">
+                <span class="edit">
+                    <span class="material-icons">edit</span>
+                </span>
+                <span class="delete" onclick="removeThisTask (event)">
+                    <span class="material-icons">delete_forever</span>
+                </span>
+            </div>
+        </li>
+        `
+        listBody.insertAdjacentHTML ('afterbegin', liTask)
+
+    })
+    taskList.innerHTML = ''
     taskList.append (listBody)
 }
 
 function removeThisTask (event) {
-    event.target.parentElement.parentElement.parentElement.remove ()
+    let liElemForRemove = event.target.parentElement.parentElement.parentElement
+    // console.log(liElemForRemove)
+    // liElemForRemove.remove ()
+
+    let itemForRemoveIndex = ToDoArray.findIndex (function (item) {
+        return item.id == liElemForRemove.dataset.id    // Why == works but === not works?!
+    })
+    console.log(itemForRemoveIndex)
+    ToDoArray.splice (itemForRemoveIndex, 1)
+    addToLocalStorage (ToDoArray)
+    toDoGenerator (ToDoArray)
 }
+
+/**
+ * Window onload
+ */
+window.addEventListener('load', function () {
+    toDoGenerator (getFromLocalStorage ())
+})
 
 /**
  * Add events
