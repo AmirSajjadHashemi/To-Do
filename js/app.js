3fe4f579f5e324fea5b71a9f67609c195363715e
DariushGreat <amirsajjad.hashemi@outlook.com>
2023-02-28 19:59:59 +0330

    Adding first localstorage / create todo array /

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ let taskList = _query ('.tasks-list')
 let addNewBTN = _query ('#add-new')
 let addNewField = _query ('#add-new-field')
 let addNewFieldInput = _query ('#add-new-field input')
+let ToDoArray = []
+let count = 1
 
 /**
  * Functions
@@ -19,15 +21,27 @@ function openAddNewInput () {
     addNewField.classList.remove ('hide')
 }
 
-function addToDoHandler (input_string) {
+function addToDoHandler () {
+    let newTask = {
+        id: count,
+        title: addNewFieldInput.value,
+        status: 'incomplete',
+    }
+    ToDoArray.push (newTask)
+    localStorage.setItem ('to-do', JSON.stringify(ToDoArray))
+    toDoGenerator (addNewFieldInput.value, ToDoArray[count-1].id, ToDoArray[count-1].status)
+    count ++
+}
+
+function toDoGenerator (input_string, id, status) {
     addNewField.classList.add ('hide')
     addNewBTN.classList.remove ('hide')
     let listBody = $.createElement ('div')
-    listBody.className = 'bod y'
+    listBody.className = 'body'
     let liTask = `
-    <li class="task">
+    <li class="task" id="${id}" status="${status}">
         <span class="material-icons">drag_indicator</span>
-        <h4>${addNewFieldInput.value}</h4>
+        <h4>${input_string}</h4>
         <div class="editable">
             <span class="edit">
                 <span class="material-icons">edit</span>
@@ -40,11 +54,6 @@ function addToDoHandler (input_string) {
     `
     listBody.insertAdjacentHTML ('afterbegin', liTask)
     taskList.append (listBody)
-
-    let deleteTask = _query ('.delete')
-    deleteTask.addEventListener ('click', function () {
-        console.log ('hi')
-    })
 }
 
 function removeThisTask (event) {
